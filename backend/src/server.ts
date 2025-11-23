724b1ac0fa5b4ed3405aed3a76700602e615298a
Njenga Ian Josh <joshnjenga066@gmail.com>
2025-11-23 18:31:12 +0300

    First touches t

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -88,7 +88,7 @@ const startServer = async () => {
     
     app.listen(config.port, () => {
       console.log(`🚀 Server running on port ${config.port} in ${config.env} mode`);
-      console.log(config.mongo);
+      console.log(config.cors);
 
     });
   } catch (error) {
